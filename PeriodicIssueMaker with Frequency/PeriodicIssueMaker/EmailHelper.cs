1cc7f735a172610cdeb347ba34fc065cfda7db99
By161 <92064135+By161@users.noreply.github.com>
2023-11-21 16:29:26 -0500

    added frequency check and removed email

---
+++ PeriodicIssueMaker with Frequency/PeriodicIssueMaker/EmailHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Exchange.WebServices.Data;
+using Microsoft.Identity.Client;
+using PeriodicIssueMaker.Properties;
+
+namespace PeriodicIssueMaker
+{
+    public enum EmailBodyTypeEnum : byte
+    {
+        HTML = 0,
+        Text = 1
+    }
+    public class EmailHelper
+    {        
+        public string EmailFromAddress { get; set; }
+
+
+        public string SmtpAccount { get; set; }
+
+
+
+        /// <summary>
+        /// send email using the new MS auth (though still ews and not MS Graph yet)
+        /// </summary>
+        /// <param name="args">send email arguments</param>
+        /// <returns>task which would contain result of email send request</returns>
+        /// <remarks>TODO:  replace ews with MS Graph API</remarks>
+        public async System.Threading.Tasks.Task SendEmail(SendEmailArgs args)
+        {
+            var ewsClient = new ExchangeService();
+            
+            var cca = ConfidentialClientApplicationBuilder
+                        .Create(Settings.Default.NewAuthClientId)
+                        .WithClientSecret(Settings.Default.NewAuthClientSecret)
+                        .WithTenantId(Settings.Default.NewAuthTenantId)
+                        .Build();
+            var ewsScopes = new string[] { "https://outlook.office365.com/.default" };
+            
+            var authResult = await cca.AcquireTokenForClient(ewsScopes)
+                .ExecuteAsync();
+            
+            ewsClient.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
+            ewsClient.Credentials = new OAuthCredentials(authResult.AccessToken);
+            ewsClient.ImpersonatedUserId =
+                new ImpersonatedUserId(ConnectingIdType.SmtpAddress, Settings.Default.NewAuthImpersonatedUserId);
+            
+            //Include x-anchormailbox header
+            ewsClient.HttpHeaders.Add("X-AnchorMailbox", Settings.Default.NewAuthAnchorMailbox);
+            var message = new EmailMessage(ewsClient);
+            message.Subject = args.Subject;
+            message.Body = args.Body;
+            message.Body.BodyType = args.BodyType.HasValue ? (BodyType)args.BodyType : BodyType.HTML;
+
+            message.From =
+                new EmailAddress(new string[] { Settings.Default.NewAuthAnchorMailbox, EmailFromAddress, SmtpAccount }
+                    .First(s => !string.IsNullOrWhiteSpace(s)));
+
+            message.ToRecipients.AddRange(args.ToAddresses);
+            message.Send();
+
+        }
+
+    }
+}
---
+++ PeriodicIssueMaker with Frequency/PeriodicIssueMaker/Program.cs
@@ -0,0 +1,270 @@
+﻿/* Periodic Issue Maker
+ * Reads in a job file that lists issues to be created and calls the stored procedure to insert issues according to the job configuration file.
+ * Author: Brandon Yuen
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ApplicationBlocks.Data;
+using PeriodicIssueMaker.Properties;
+
+namespace PeriodicIssueMaker
+{
+    internal class Program
+    {
+        static int issueNumber = 0;
+        static string emailBody = "";
+        static Dictionary<string, int> DaysOfWeek = new Dictionary<string, int>() 
+        { { "monday", 1 }, { "tuesday", 2 }, { "wednesday", 3 }, { "thursday", 4 }, { "friday", 5 }, { "saturday", 6 }, { "sunday", 0 }, };
+        private static async Task Main(string[] args)
+        {
+            //program starts
+            await SendEmail(Settings.Default.CreatorEmail, Settings.Default.RunMessage, Settings.Default.RunMessage);
+            LogMessage(Settings.Default.ProgramStart, Settings.Default.ProgramStartAndEnd);
+            using (StreamReader read = new StreamReader(Settings.Default.FilePath))
+            {
+                //checks if file is valid
+                if (FileValid(Settings.Default.FilePath))
+                {
+                    try
+                    {
+                        ProcessIssueJobFile();
+                        await SendEmail(Settings.Default.CreatorEmail, Settings.Default.Subject, emailBody);
+                    }
+                    catch (Exception ex)
+                    {
+                        await SendEmail(Settings.Default.CreatorEmail, Settings.Default.ErrorSubject, ex.Message);
+                    }
+                }
+                //file is invalid
+                else
+                {
+                    await SendEmail(Settings.Default.CreatorEmail, Settings.Default.ErrorSubject, LogMessage(Settings.Default.InvalidFileMessage, Settings.Default.ProgramStartAndEnd));
+                }
+            }
+        }
+
+        //helper method to call the stored procedure
+        private static DataSet CallSproc(string[] input, DateTime dueDate)
+        {
+            DataSet ds = SqlHelper.ExecuteDataset(Settings.Default.IssueTrackerConnectionString, CommandType.StoredProcedure, "InsertBugAndComment",
+                                new SqlParameter("@bg_short_desc", input[4]),
+                                new SqlParameter("@bg_reported_user", input[5]),
+                                new SqlParameter("@bg_priority", input[6]),
+                                new SqlParameter("bg_category", input[7]),
+                                new SqlParameter("@bg_project", input[8]),
+                                new SqlParameter("@bg_status", input[9]),
+                                new SqlParameter("bg_assigned_to_user", input[10]),
+                                new SqlParameter("bg_org", input[11]),
+                                new SqlParameter("Date_Due", dueDate),
+                                new SqlParameter("bg_tags", input[13]),
+                                new SqlParameter("@Comment", input[14]));
+            return ds;
+        }
+
+        //helper method to send the email
+        private static async Task SendEmail(string emailAddress, string Subject, string EmailMessage)
+        {
+            SendEmailArgs sendEmailArgs = new SendEmailArgs(emailAddress, Subject, EmailMessage);
+            EmailHelper emailHelper = new EmailHelper();
+            await emailHelper.SendEmail(sendEmailArgs);
+        }
+        //helper methods to compare the input date to current date and return a DateTime based off input string
+        //gets the date of the last specified day in the month
+        public static DateTime LastDay(int year, int month, string day)
+        {
+            DateTime dt;
+            dt = new DateTime(year, month, DateTime.DaysInMonth(year, month), System.Globalization.CultureInfo.CurrentCulture.Calendar);
+            int daysOffset = Convert.ToInt32(dt.DayOfWeek) - DaysOfWeek[day];
+            if (daysOffset < 0) daysOffset += 7;
+            dt = dt.AddDays(-daysOffset);
+            return dt;
+        }
+        //gets the date of the first specified day in the month
+        public static DateTime FirstDay(int year, int month, string day)
+        {
+            DateTime dt;
+            dt = new DateTime(year, month, 1, System.Globalization.CultureInfo.CurrentCulture.Calendar);
+            int daysOffset = DaysOfWeek[day] - Convert.ToInt32(dt.DayOfWeek);
+            if (daysOffset < 0) daysOffset += 7;
+            dt = dt.AddDays(daysOffset);
+            return dt;
+        }
+        //helper method to make log messages
+        private static string LogMessage(string message, int logLevel)
+        {
+            if (logLevel >= Settings.Default.LogLevel)
+            {
+                using (StreamWriter streamWriter = new StreamWriter(Settings.Default.LogPath, true))
+                {
+                    {
+                        streamWriter.WriteLine(DateTime.Now + Settings.Default.Colon + message);
+                    }
+                }
+            }
+            Console.WriteLine(DateTime.Now + Settings.Default.Colon + message);
+            return (DateTime.Now + Settings.Default.Colon + message);
+        }
+        //helper method to check if the job file is valid
+        private static bool FileValid(string file)
+        {
+            var linesRead = File.ReadLines(Settings.Default.FilePath);
+            foreach (var lineRead in linesRead)
+            {
+                lineRead.Trim();
+                //line is whitespace
+                if (string.IsNullOrWhiteSpace(lineRead))
+                {
+                    continue;
+                }
+                //line is a comment
+                else if (lineRead.StartsWith(Settings.Default.CommentString)) 
+                {
+                    continue;
+                }
+                //line is proper command
+                string[] inputArr = lineRead.Split(Settings.Default.StringSplitArg);
+                if ((inputArr.Length != 15) || (!(IsInt(inputArr[0]) && IsInt(inputArr[3]) && IsInt(inputArr[5]) &&
+                    IsInt(inputArr[6]) && IsInt(inputArr[7]) && IsInt(inputArr[8])
+                    && IsInt(inputArr[9]) && IsInt(inputArr[10]) && IsInt(inputArr[11]))))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        //helper method to check if input is a number
+        private static bool IsInt(string input)
+        {
+            return int.TryParse(input, out int result);
+        }
+
+        //helper method to check if an inputted string is a valid email address
+        private static bool IsValidEmail(string email)
+        {
+            if (email.Contains(Settings.Default.StringSplitArg))
+            {
+                string[] emails = email.Split(Settings.Default.StringSplitArg);
+                for (int i = 0; i < emails.Length; i++)
+                {
+                    if (!emails[i].Contains("@") || emails[i].IndexOf("@") < 0 || emails[i].IndexOf("@") >= emails[i].Length)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        //create and insert issue
+        private static async void ProcessIssueJobFile()
+        {
+            //iterates through each line 
+            //program already ensured the file has correct amount of elements by calling FileValid in the main method
+            //so when the input is split into an array, there will be no out of bounds error.
+            var linesRead = File.ReadLines(Settings.Default.FilePath);
+            List<SendEmailArgs> emailList = new List<SendEmailArgs>();
+            foreach (var lineRead in linesRead)
+            {
+                if (!string.IsNullOrWhiteSpace(lineRead) && !(lineRead.StartsWith(Settings.Default.CommentString)))
+                {
+                    string[] inputArr = lineRead.Split(Settings.Default.StringSplitArg);
+                    string dueDay = inputArr[12].Substring(inputArr[12].IndexOf(" ") + 1).ToLower();
+                    string prefix = inputArr[12].Substring(0, inputArr[12].IndexOf(" "));
+                    string emailRecipients = Settings.Default.CreatorEmail;
+                    DateTime dueDate;
+                    if (prefix == Settings.Default.FirstPrefix)
+                    {
+                        dueDate = FirstDay(DateTime.Now.Year, DateTime.Now.Month, dueDay);
+                    }
+                    else
+                    {
+                        dueDate = LastDay(DateTime.Now.Year, DateTime.Now.Month, dueDay);
+                    }
+                    //bot starts to run
+                    try
+                    {
+                        //if (CheckMostRecentJobDate(inputArr[0], inputArr[2]))
+                        {
+                            DataSet ds = CallSproc(inputArr, dueDate);
+                            issueNumber = Convert.ToInt32(ds.Tables[0].Rows[0][0].ToString());
+                            if (!(emailRecipients.Contains(inputArr[15])))
+                            {
+                                emailRecipients += Settings.Default.Semicolon + inputArr[15];
+                            }
+                            emailBody += Settings.Default.TestingURL + issueNumber + Settings.Default.NewLine;
+                            SendEmailArgs email = new SendEmailArgs(Settings.Default.CreatorEmail, Settings.Default.Subject + issueNumber, Settings.Default.TestingURL + issueNumber);
+                            emailList.Add(email);
+                        }
+                    }
+                    //bot failed to insert issue
+                    catch (SqlException)
+                    {
+                        await SendEmail(Settings.Default.CreatorEmail, Settings.Default.ErrorSubject, lineRead + Settings.Default.NewLine + inputArr[4]);
+                    }
+                    catch (Exception ex)
+                    {
+                        await SendEmail(Settings.Default.CreatorEmail, Settings.Default.ErrorSubject, ex + Settings.Default.NewLine + lineRead + Settings.Default.NewLine + inputArr[4]);
+                    }
+                }
+            }
+            await SendIndividualEmails(emailList);
+        }
+        //loop to asynchronously send all email notices individually
+        private static async Task SendIndividualEmails(List<SendEmailArgs> emailList)
+        {
+            List<Task> listOfTasks = new List<Task>();
+            foreach (SendEmailArgs email in emailList)
+            {
+                listOfTasks.Add(SendEmail(string.Join(";", email.ToAddresses), email.Subject, email.Body));
+            }
+            await Task.WhenAll(listOfTasks);
+        }
+
+        //return true if required amount of days from inputted frequency has passed today
+        //private static bool CheckMostRecentJobDate(string jobID, string Frequency)
+        //{
+        //    DataSet ds = SqlHelper.ExecuteDataset(Settings.Default.IssueTrackerConnectionString, CommandType.StoredProcedure, "GetJobMostRecentCompletionTimeStamp",
+        //                        new SqlParameter("@JobId", jobID));
+        //    DateTime dateTime = (DateTime)ds.Tables[0].Rows[0][0];
+        //    int DaysSinceLastJob = (int)(DateTime.Now - dateTime).TotalDays;
+        //    Frequency.ToLower();
+        //    if(dateTime.Year == 1)
+        //    {
+        //        return true;
+        //    }
+        //    switch (Frequency)
+        //    {
+        //        case "weekly":
+        //            if (DaysSinceLastJob < 7)
+        //            {
+        //                return false;
+        //            }
+        //            else break;
+        //        case "monthly":
+        //            if(DaysSinceLastJob < 30)
+        //            {
+        //                return false;
+        //            }
+        //            else break;
+                    
+        //        case "yearly":
+        //            if(DaysSinceLastJob < 365)
+        //            {
+        //                return false;
+        //            }
+        //            else break;
+                    
+        //        default: return true;
+
+        //    }
+        //    return true;
+        //}
+    }
+}
+
